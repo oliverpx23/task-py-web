0390675f3507349228d1146a0fec8043125f253d
Oliver Pots <oliverpx@gmail.com>
2024-04-15 03:49:37 -0400

    refactor: tasklist page

--- src/pages/TaskListPage.tsx
+++ src/pages/TaskListPage.tsx
@@ -1,6 +1,53 @@
+import { PageContainer } from "@/core/components";
+import { useTaskStore } from "@/store/task.store";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue } from "@nextui-org/react";
+
+
 
 export const TaskListPage = () => {
+
+  const tasks = useTaskStore(state => state.tasks);
+
+  const columns = [
+    {
+      key: 'id',
+      label: 'ID',
+    },
+    {
+      key: 'name',
+      label: 'NAME'
+    },
+    {
+      key: 'description',
+      label: 'DESCRIPTION',
+    },
+    {
+      key: 'status',
+      label: 'STATUS',
+    }
+  ];
+  
+
+
+
   return (
-    <div>TaskListPage</div>
-  )
-}
+    <PageContainer>
+      <Table aria-label="Example table with dynamic content">
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn key={column.key}>{column.label}</TableColumn>
+          )}
+        </TableHeader>
+        <TableBody items={tasks}>
+          {(item) => (
+            <TableRow key={`task-${item.id}`}>
+              {(columnKey) => (
+                <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </PageContainer>
+  );
+};
