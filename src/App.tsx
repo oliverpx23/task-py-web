85a9f3db6696bc9a927bf629adb3d82c25ef33f1
Oliver Pots <oliverpx@gmail.com>
2024-04-14 22:31:12 -0400

    add: next ui config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./styles/app.css";
+import {Button} from "@nextui-org/react";
 
 function App() {
   return (
     <div className="flex justify-center items-center h-screen w-screen">
       <div className="h-40 w-40 bg-blue-500 text-white font-bold">
         <h1>hello vite</h1>
+        <Button color="primary">
+          Button
+        </Button>
       </div>
     </div>
   );
