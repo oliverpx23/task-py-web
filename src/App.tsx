30a7d0f5d05e6ae6e6c393c1ffdd8513908685ea
Oliver Pots <oliverpx@gmail.com>
2024-04-14 22:15:11 -0400

    add: Test utility tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import "./styles/app.css";
 
 function App() {
   return (
-    <div>
-      <h1>hello vite</h1>
+    <div className="flex justify-center items-center h-screen w-screen">
+      <div className="h-40 w-40 bg-blue-500 text-white font-bold">
+        <h1>hello vite</h1>
+      </div>
     </div>
   );
 }
