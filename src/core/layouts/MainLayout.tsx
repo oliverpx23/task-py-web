92fcbe62a86e5f5c3ae44e66ee298c2a4e31c083
Oliver Pots <oliverpx@gmail.com>
2024-04-17 14:36:42 -0400

    add: task from api

--- src/core/layouts/MainLayout.tsx
+++ src/core/layouts/MainLayout.tsx
@@ -2,8 +2,19 @@
 import { Outlet } from "react-router-dom";
 import { NavbarWrapper } from "@/core/components/NavbarWrapper";
 import { SidebarWrapper } from "@/core/components";
+import { useTaskStore } from "@/store";
+import { useEffect } from "react";
 
 export const MainLayout = () => {
+
+  const getTasks = useTaskStore(state => state.getTasks);
+
+  useEffect(() => {
+    console.log('layout effect')
+    getTasks();
+  }, [getTasks]);
+  
+
 
   return (
     <section className="flex">
