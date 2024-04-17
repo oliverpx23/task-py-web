c661003b314312bb0d209b395343229d7dfb9f50
Oliver Pots <oliverpx@gmail.com>
2024-04-17 00:52:07 -0400

    fix: dynamic styles tailwind

--- src/core/components/kanban/Board.tsx
+++ src/core/components/kanban/Board.tsx
@@ -4,7 +4,6 @@ import { Button, ScrollShadow, Spacer } from "@nextui-org/react";
 import { clsx } from "clsx";
 import { TaskCard } from "./TaskCard";
 import { useState, DragEvent } from "react";
-import { classColorMap } from "@/core/config/constants";
 
 interface Props {
   status: TaskStatus;
@@ -36,14 +35,22 @@ export const Board = ({ status }: Props) => {
       }}
       onDrop={handleDrop}
       className={clsx([
-        `flex flex-col flex-shrink-0 w-72 rounded-md overflow-hidden border border-${classColorMap[status]}`,
+        `flex flex-col flex-shrink-0 w-72 rounded-md overflow-hidden border`,
+        status == 'pending' && "border-warning-800",
+        status == 'in-progress' && "border-primary-800",
+        status == 'done' && "border-success-800",
         isDragging && "border-4 border-dotted",
-        (isDragging && onDragOver) && `border-4 bg-${classColorMap[status]}/20`,
+        (isDragging && onDragOver) &&  status == 'pending' && `border-4 bg-warning-800/20`,
+        (isDragging && onDragOver) &&  status == 'in-progress' && `border-4 bg-primary-800/20`,
+        (isDragging && onDragOver) &&  status == 'done' && `border-4 bg-success-800/20`,
       ])}
     >
       <div
         className={clsx([
-          `flex items-center flex-shrink-0 h-10 p-4 bg-${classColorMap[status]} dark:bg-${classColorMap[status]}`,
+          `flex items-center flex-shrink-0 h-10 p-4`,
+          status == 'pending' && "bg-warning-800",
+          status == 'in-progress' && "bg-primary-800",
+          status == 'done' && "bg-success-800",
         ])}
       >
         <span className="block text-sm font-semibold uppercase">
@@ -60,7 +67,10 @@ export const Board = ({ status }: Props) => {
 
       <Button 
         className={clsx([
-          `rounded-md border-${classColorMap[status]}`,
+          `rounded-md`,
+          status == 'pending' && "border-warning-800",
+          status == 'in-progress' && "border-primary-800",
+          status == 'done' && "border-success-800",
         ])}
         variant="ghost"
       >
