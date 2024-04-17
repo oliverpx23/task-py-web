9600ed558ef9a9c30d740da27760e54237ba4938
Oliver Pots <oliverpx@gmail.com>
2024-04-17 14:34:49 -0400

    refactor: task store

--- src/store/task.store.ts
+++ src/store/task.store.ts
@@ -1,38 +1,42 @@
-import { fakeUsers } from "@/core/config/fake-users";
-import { Task, TaskStatus } from "@/interfaces";
+import { Task, TaskDto, TaskStatus } from "@/interfaces";
+import { TasksService } from "@/services/task.service";
 import { StateCreator, create } from "zustand";
-import { devtools, persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 interface TaskState {
+  isLoading: boolean;
+  isSaving: boolean;
   draggingTaskId?: number;
   tasks: Task[];
 }
 
 interface TaskActions {
+  getTasks: () => void;
   getTaskByStatus: (status: TaskStatus) => Task[];
   getTaskById: (taskId: number) => Task | undefined;
   setDraggingTaskId: (taskId: number) => void;
   removeDraggingTaskId: () => void;
   changeStatus: (taskId: number, status: TaskStatus) => void;
   onTaskDrop: (status: TaskStatus) => void;
-  // addTask: (title: string, status: TaskStatus) => void;
+  addTask: (task: TaskDto) => Promise<void> | void;
+  deleteTask: (taskId: number | string) => void;
 }
 
-type TaskStore = TaskState & TaskActions;
-type TaskMiddlewares = [["zustand/devtools", never], ["zustand/immer", never]];
+export type TaskStore = TaskState & TaskActions;
+type TaskMiddlewares = [["zustand/immer", never]];
 
 const storeApi: StateCreator<TaskStore, TaskMiddlewares> = (set, get) => ({
+  isLoading: false,
+  isSaving: false,
   draggingTaskId: undefined,
-  tasks: [
-    { id: 1, status: 'pending', name: 'Login page', description: 'Desarrollar pagina de login con tailwind css', assignedTo: fakeUsers[0] },
-    { id: 2, status: 'in-progress', name: 'Dashboard page', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad nam aliquam temporibus cum rerum placeat pariatur ab iusto deleniti, cupiditate quibusdam reiciendis magni non optio vitae sit saepe excepturi omnis!', assignedTo: fakeUsers[1] },
-    { id: 3, status: 'done', name: 'Register page', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad nam aliquam temporibus cum rerum placeat pariatur ab iusto deleniti, cupiditate quibusdam reiciendis magni non optio vitae sit saepe excepturi omnis! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad nam aliquam temporibus cum rerum placeat pariatur ab iusto deleniti, cupiditate quibusdam reiciendis magni non optio vitae sit saepe excepturi omnis!', assignedTo: fakeUsers[0] },
-    { id: 4, status: 'pending', name: 'Print page', description: 'Desarrollar pagina de login con tailwind css', assignedTo: fakeUsers[1] },
-    { id: 5, status: 'in-progress', name: 'Report page', description: 'Desarrollar pagina de login con tailwind css', assignedTo: fakeUsers[0] },
-    { id: 6, status: 'pending', name: 'Commit to github', description: 'Desarrollar pagina de login con tailwind css', assignedTo: fakeUsers[1] },
+  tasks: [],
 
-  ],
+  getTasks: async () => {
+    set({ isLoading: true });
+    const tasks = await TasksService.getTasks();
+    if (!tasks) return;
+    set({ tasks, isLoading: false });
+  },
 
   getTaskByStatus: (status: TaskStatus) => {
     return get().tasks.filter((task) => task.status === status);
@@ -51,14 +55,31 @@ const storeApi: StateCreator<TaskStore, TaskMiddlewares> = (set, get) => ({
     set({ draggingTaskId: undefined });
   },
 
-  changeStatus: (taskId, status) => {
+  changeStatus: async (taskId, status) => {
+
     const selectedTask = get().getTaskById(taskId);
     if(!selectedTask) return;
     selectedTask.status = status;
-    set({ tasks: get().tasks.map(task => {
-      if(task.id != selectedTask.id) return task;
-      return selectedTask;
-    }) });
+
+    set({ 
+      tasks: get().tasks.map(task => {
+        if(task.id != selectedTask.id) return task;
+        return selectedTask;
+      }) 
+    });
+
+    set({ isSaving: true });
+
+    const updatedTask = await TasksService.updateTask({
+      ...selectedTask,
+      status
+    });
+
+    if(!updatedTask) return;
+    get().getTasks();
+
+    set({ isSaving: false });
+
   },
 
   onTaskDrop: (status: TaskStatus) => {
@@ -69,15 +90,27 @@ const storeApi: StateCreator<TaskStore, TaskMiddlewares> = (set, get) => ({
     }
   },
 
-  // addTask: (title: string, status: TaskStatus) => {
-  //   const newTask = { id: uuidV4(), title, status };
-  //   set((state) => {
-  //     state.tasks[newTask.id] = newTask;
-  //   });
-  // },
+  addTask: async (task: TaskDto) => {
+    set({ isSaving: true });
+    const newTask = await TasksService.createTask(task);
+    if (newTask) {
+      get().getTasks();
+    }
+    set({ isSaving: false });
+
+  },
+
+  deleteTask: async (taskId: number | string) => {
+    set({ isSaving: true });
+    const deletedTask = await TasksService.deleteTask(taskId);
+    if (deletedTask) {
+      get().getTasks();
+    }
+    set({ isSaving: false });
+  }
 
 });
 
 export const useTaskStore = create<TaskStore>()(
-  devtools(persist(immer(storeApi), { name: "task-storage" }))
+  immer(storeApi)
 );
