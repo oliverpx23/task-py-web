38d7498f6b9d381de5831dcd4bed14ad495bae2d
Oliver Pots <oliverpx@gmail.com>
2024-04-17 14:38:44 -0400

    add: env template

--- src/core/api/taskpy.apiclient.ts
+++ src/core/api/taskpy.apiclient.ts
@@ -2,9 +2,8 @@ import axios from 'axios';
 
 
 const taskpyApi = axios.create({
-  baseURL: import.meta.env.VITE_ANALYTICS_KEY ?? 'https://taskpy-api.up.railway.app'
+  baseURL: import.meta.env.VITE_API_URL ?? 'https://taskpy-api.up.railway.app'
 });
-
 
 
 export {
