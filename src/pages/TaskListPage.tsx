1b900e3581bf4053348570af192c5bf13b1e81c0
Oliver Pots <oliverpx@gmail.com>
2024-04-17 14:35:25 -0400

    add: tasklist loader

--- src/pages/TaskListPage.tsx
+++ src/pages/TaskListPage.tsx
@@ -1,12 +1,23 @@
 import { PageContainer } from "@/core/components";
 import { useTaskStore } from "@/store/task.store";
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue } from "@nextui-org/react";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue, CircularProgress } from "@nextui-org/react";
 
 
 
 export const TaskListPage = () => {
 
   const tasks = useTaskStore(state => state.tasks);
+  const isLoading = useTaskStore(state => state.isLoading);
+
+  if (isLoading || tasks.length === 0) {
+    return (
+      <PageContainer>
+        <div className="flex justify-center items-center h-full w-full">
+          <CircularProgress size="lg" aria-label="cargando..." />
+        </div>
+      </PageContainer>
+    );
+  }
 
   const columns = [
     {
@@ -32,22 +43,24 @@ export const TaskListPage = () => {
 
   return (
     <PageContainer>
-      <Table aria-label="Example table with dynamic content">
-        <TableHeader columns={columns}>
-          {(column) => (
-            <TableColumn key={column.key}>{column.label}</TableColumn>
-          )}
-        </TableHeader>
-        <TableBody items={tasks}>
-          {(item) => (
-            <TableRow key={`task-${item.id}`}>
-              {(columnKey) => (
-                <TableCell>{getKeyValue(item, columnKey)}</TableCell>
-              )}
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+      <div className="mx-auto">
+        <Table aria-label="Example table with dynamic content">
+          <TableHeader columns={columns}>
+            {(column) => (
+              <TableColumn key={column.key}>{column.label}</TableColumn>
+            )}
+          </TableHeader>
+          <TableBody items={tasks}>
+            {(item) => (
+              <TableRow key={`task-${item.id}`}>
+                {(columnKey) => (
+                  <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+                )}
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
     </PageContainer>
   );
 };
