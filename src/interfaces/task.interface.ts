a1b8179d8767e2f197ebd123c6bb61441ef7346d
Oliver Pots <oliverpx@gmail.com>
2024-04-17 14:35:53 -0400

    refactor: task interface

--- src/interfaces/task.interface.ts
+++ src/interfaces/task.interface.ts
@@ -6,6 +6,16 @@ export interface Task {
     id: number;
     name: string;
     description: string;
-    assignedTo: User;
+    "assigned_to": number;
+    "assigned_to_user": User;
+    status: TaskStatus
+}
+
+
+export interface TaskDto {
+    id?: number;
+    name: string;
+    description: string;
+    "assigned_to": number;
     status: TaskStatus
 }
--- src/pages/Error404Page.tsx
+++ src/pages/Error404Page.tsx
@@ -1,6 +1,6 @@
 
 export const Error404Page = () => {
   return (
-    <div>Error404Page</div>
+    <div>Error404</div>
   )
 }
