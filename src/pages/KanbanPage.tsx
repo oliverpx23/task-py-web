e188e0cbb4e302f9677e09cad3387087e2be4af9
Oliver Pots <oliverpx@gmail.com>
2024-04-17 14:35:37 -0400

    refactor: kanban page

--- src/pages/KanbanPage.tsx
+++ src/pages/KanbanPage.tsx
@@ -1,14 +1,28 @@
 import { PageContainer } from "@/core/components";
-import { Board } from "@/core/components/kanban";
+import { Board, NewTaskModal } from "@/core/components/kanban";
+import { useNewTaskModal } from "@/hooks";
 
 export const KanbanPage = () => {
 
+  const { 
+    isOpen, 
+    onOpen,
+    onOpenChange, 
+    newTaskStatus, 
+    handleOpenNewTaskModal 
+  } = useNewTaskModal();
 
   return (
     <PageContainer center>
-      <Board status="pending" />
-      <Board status="in-progress" />
-      <Board status="done"/>
+      <NewTaskModal
+        newTaskStatus={newTaskStatus}
+        isOpen={isOpen}
+        onOpen={onOpen}
+        onOpenChange={onOpenChange}
+      />
+      <Board openNewTaskModal={handleOpenNewTaskModal} status="pending" />
+      <Board openNewTaskModal={handleOpenNewTaskModal} status="in-progress" />
+      <Board openNewTaskModal={handleOpenNewTaskModal} status="done" />
     </PageContainer>
   );
 };
